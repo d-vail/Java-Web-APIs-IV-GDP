fdfd34bf9eb82ed5bedff85aa90057903d40c0ac
Danny Vail <danny_vail@outlook.com>
2019-02-14 17:51:45 -0500

    CREATE A RUNTIME EXCEPTION FOR COUNTRY In CountryNotFoundException.java - Extend the RuntimeException class and pass a custom message when country id is invalid

---
+++ src/main/java/com/lambdaschool/gdp/country/CountryNotFoundException.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.gdp.country;
+
+/**
+ * CountryNotFoundException is a custom runtime exception to be thrown when an invalid country id is
+ * provided.
+ */
+public class CountryNotFoundException extends RuntimeException {
+  /**
+   * Constructor
+   *
+   * Pass custom message to RuntimeException class.
+   *
+   * @param id  A country id
+   */
+  public CountryNotFoundException(Long id) {
+    super("Could not find country");
+  }
+}
