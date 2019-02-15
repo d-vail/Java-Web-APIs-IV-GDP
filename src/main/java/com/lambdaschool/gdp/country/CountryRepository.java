8f7c826ded102fce5bd4e31999a087f2b8e9915a
Danny Vail <danny_vail@outlook.com>
2019-02-14 19:23:39 -0500

    ADD QUERIES TO COUNTRY DB INTERFACE In countryRepository.java - Add a JPQL query to get the sum of all gdps - Add a named query to find all countries by country name

--- src/main/java/com/lambdaschool/gdp/country/CountryRepository.java
+++ src/main/java/com/lambdaschool/gdp/country/CountryRepository.java
@@ -1,9 +1,14 @@
 package com.lambdaschool.gdp.country;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * The CountryRepository is an interface for interacting with the database.
  */
 public interface CountryRepository extends JpaRepository<Country, Long> {
+  @Query("select sum(gdp) from Country")
+  Long getGDPSum();
+
+  Country findByCountry(String country);
 }
